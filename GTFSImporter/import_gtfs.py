b95a64427eda7ce11a5b23a1d54ce7b9a49e2896
Yaogang Lian <ylian@happenapps.com>
2011-10-10 20:04:47 -0400

    Create the route_stops table to store stop sequences for each route. Drop stop_times and trips tables from the database after importing.

--- GTFSImporter/import_gtfs.py
+++ GTFSImporter/import_gtfs.py
@@ -1,6 +1,9 @@
-import csv, sqlite3, codecs
+import csv, sqlite3, codecs, os
 
 class UnicodeCsvReader(object):
+    """
+    A wrapper class to add unicode support to the csv module
+    """
     def __init__(self, f, encoding="utf-8", **kwargs):
         self.csv_reader = csv.reader(f, **kwargs)
         self.encoding = encoding
@@ -18,7 +21,13 @@ class UnicodeCsvReader(object):
     def line_num(self):
         return self.csv_reader.line_num
 
-def createTable(curs, table_name, table_columns, filename):
+
+def createTable(curs, table_name, table_columns, filename, source_id):
+    """
+    Create tables, then popolate them with data from GTFS files.
+    """
+    primary_keys = ("route_id", "stop_id", "trip_id")
+    
     #curs.execute('drop table if exists %s' % table_name)
     curs.execute('create table if not exists %s (%s)' % (table_name, table_columns))
     
@@ -43,33 +52,84 @@ def createTable(curs, table_name, table_columns, filename):
         for i in range(len(row)):
             if header[i] in column_names:
                 idx = column_names.index(header[i])
-                new_row[idx] = row[i]
+                if header[i] in primary_keys:
+                    new_row[idx] = str(source_id) + '#' + row[i]
+                else:
+                    new_row[idx] = row[i]
         new_data.append(new_row)
     
     curs.executemany("insert into %s (%s) values (%s)" % (table_name, ','.join(column_names), ','.join(['?']*len(column_names))), new_data)
-
+    
+    # # Create a unique index from the primary key
+    # if primary_key is not None:
+    #     curs.execute('create unique index %s on %s (%s)' % ('idx_'+primary_key, table_name, primary_key))
+    
 
 # __main__
-for folder in ("google_transit_UVA", "google_transit_Charlottesville"):
-    conn = sqlite3.connect("HoosBus-GTFS.db")
-    curs = conn.cursor()
 
+# We are combining two sources into one database so we need a source_id.
+source_id = 0
+
+# Creat a new database and connect to it
+database_name = "HoosBus-GTFS.db"
+
+if os.path.exists(database_name):
+    os.remove(database_name)
+
+conn = sqlite3.connect(database_name)
+curs = conn.cursor()
+
+for folder in ("google_transit_UVA", "google_transit_Charlottesville"):
     # Creat the routes table
     table_columns = "route_id TEXT, agency_id TEXT, route_short_name TEXT, route_long_name TEXT"
-    createTable(curs, 'routes', table_columns, "routes.txt")
-
+    createTable(curs, 'routes', table_columns, "routes.txt", source_id)
+    
     # Create the stops table
-    table_columns = "stop_id TEXT, stop_code TEXT, stop_name TEXT, stop_desc TEXT, stop_lat REAL, stop_lon REAL"
-    createTable(curs, 'stops', table_columns, "stops.txt")
-
+    table_columns = "stop_id TEXT, stop_code TEXT, stop_name TEXT, stop_lat REAL, stop_lon REAL"
+    createTable(curs, 'stops', table_columns, "stops.txt", source_id)
+    
     # Create the trips table
-    table_columns = "route_id TEXT, service_id TEXT, trip_id TEXT, trip_headsign TEXT, trip_short_name TEXT, direction_id INTEGER"
-    createTable(curs, 'trips', table_columns, "trips.txt")
+    table_columns = "route_id TEXT, trip_id TEXT, direction_id INTEGER"
+    createTable(curs, 'trips', table_columns, "trips.txt", source_id)
+    
+    # Create the stop_times table
+    table_columns = "trip_id TEXT, stop_id TEXT, stop_sequence INTEGER"
+    createTable(curs, 'stop_times', table_columns, "stop_times.txt", source_id)
+    
+    # Since all trips in the same route have the same stop sequences, we only need to store one stop sequence per route.
+    trips_dict = {}
+    
+    for row in curs.execute('select * from trips'):
+        (route_id, trip_id, direction_id) = row
+        k = (route_id, direction_id)
+        if k not in trips_dict:
+            trips_dict[k] = trip_id
+    
+    table_columns = "route_id TEXT, direction_id INTEGER, stop_id TEXT, stop_sequence INTEGER"
+    #curs.execute('drop table if exists %s' % table_name)
+    curs.execute('create table if not exists %s (%s)' % ("route_stops", table_columns))
+    
+    route_stops = []
+    for k, trip_id in trips_dict.items():
+        for row in curs.execute('select stop_id, stop_sequence from stop_times where trip_id = "%s"'  % trip_id):
+            route_stops.append(list(k) + list(row))
+    
+    curs.executemany("insert into route_stops (route_id, direction_id, stop_id, stop_sequence) values (?, ?, ?, ?)", route_stops)
+    
+    # At last we drop the trips and stop_times tables
+    curs.execute('drop table if exists %s' % "trips")
+    curs.execute('drop table if exists %s' % "stop_times")
+    
+    # Increment the source_id
+    source_id += 1
 
-    # # Create the stop_times table
-    # table_columns = "trip_id TEXT, arrival_time TEXT, departure_time TEXT, stop_id TEXT, stop_sequence TEXT, stop_headsign TEXT"
-    # createTable(curs, 'stop_times', table_columns, "stop_times.txt")
+# Create indexes
+curs.execute('create unique index idx_stop_id on stops (stop_id)')
+curs.execute('create unique index idx_route_id on routes (route_id)')
 
-    # Commit the changes
-    conn.commit()
+# Commit the changes
+conn.commit()
+
+# Close the cursor
+curs.close()
 
